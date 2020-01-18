fe3ab1bc35db313ead31d535eecde746788474a4
hernikplays <hernik@hernikplays.cz>
2020-01-18 19:40:06 +0100

    done getting more info

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,20 +1,22 @@
 /*CODE CREATED BY HERNIKPLAYS, LICENSED UNDER GNU AGPLV3 LICENSE
 API BY JIKAN.MOE and TRACE.MOE*/
 let theme = "dark";
+
 function searchAnime(term) {
     document.getElementById("error").style.display = "none";
 
-    if(term.length <= 3){document.getElementById("error").innerHTML = "Error: You need to enter more then 3 characters!"
-    document.getElementById("error").style.display = "block";
-    return;
-}
+    if (term.length <= 3) {
+        document.getElementById("error").innerHTML = "Error: You need to enter more then 3 characters!"
+        document.getElementById("error").style.display = "block";
+        return;
+    }
     fetch(`https://api.jikan.moe/v3/search/anime?q=${term}&page=1`)
         .then(response => {
             return response.json()
         })
         .then(data => {
-            
-            if(data.results.length == 0) return console.log("Nothing found")
+
+            if (data.results.length == 0) return console.log("Nothing found")
             console.log(data.results[0])
 
             let img = document.getElementById("img")
@@ -29,48 +31,64 @@ function searchAnime(term) {
             let age = document.getElementById("age")
 
             img.src = data.results[0].image_url;
-            
+
             link.href = data.results[0].url
             syn.innerHTML = data.results[0].synopsis
             rate.innerHTML = data.results[0].score
             type.innerHTML = data.results[0].type
             age.innerHTML = data.results[0].rated
-            if(data.results[0].end_date == null){
+            if (data.results[0].end_date == null) {
                 var start = new Date(data.results[0].start_date).toLocaleDateString();
                 //start = start.split(' ').slice(0,4).join(" ")
                 startend.innerHTML = start + " / Still Airing"
-            }
-            else{
+            } else {
                 var started = new Date(data.results[0].start_date).toLocaleDateString();
                 var ended = new Date(data.results[0].end_date).toLocaleDateString();
                 startend.innerHTML = started + " / " + ended
-                
+
             }
-            if(data.results[0].airing == true){
+            if (data.results[0].airing == true) {
                 air.innerHTML = "Yes"
-            }
-            else{
+            } else {
                 air.innerHTML = "No"
             }
             ep.innerHTML = data.results[0].episodes
 
             fetch(`https://api.jikan.moe/v3/anime/${data.results[0].mal_id}`)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data.producers)
-        name.innerHTML = data.title_english + " / " + data.title_japanese
-        for (i in data.studios) {
-            document.getElementById("studio").innerHTML += data.studios[i].name;
-          } 
-         
-        
-    })
-    .catch(err => {
-        document.getElementById("error").innerHTML = "Error: " + err;
-        document.getElementById("error").style.display = "block";
-    })
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    name.innerHTML = data.title_english + " / " + data.title_japanese
+
+                    for (i in data.studios) {
+                        if (i == 0) {
+                            document.getElementById("studio").innerHTML += data.studios[i].name;
+                        } else {
+                            document.getElementById("studio").innerHTML += ", " + data.studios[i].name
+                        }
+                    }
+                    for (i in data.producers) {
+                        if (i == 0) {
+                            document.getElementById("producer").innerHTML += data.producers[i].name;
+                        } else {
+                            document.getElementById("producer").innerHTML += ", " + data.producers[i].name
+                        }
+                    }
+                    for (i in data.genres) {
+                        if (i == 0) {
+                            document.getElementById("genres").innerHTML += data.genres[i].name;
+                        } else {
+                            document.getElementById("genres").innerHTML += ", " + data.genres[i].name
+                        }
+                    }
+
+
+                })
+                .catch(err => {
+                    document.getElementById("error").innerHTML = "Error: " + err;
+                    document.getElementById("error").style.display = "block";
+                })
 
             document.getElementById("items").style.visibility = "visible";
         })
@@ -110,33 +128,33 @@ else if(theme == "light"){
     }
 }*/
 
-function searchLink(link){
+function searchLink(link) {
     fetch(`https://trace.moe/api/search?url=${link}`)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        
-        console.log(data.docs[0])
-        if(data == "No image received" || data == "Error reading imagenull"){
-            document.getElementById("error").innerHTML = "Error: Your URL is not a valid image";
-        document.getElementById("error").style.display = "block";
-        }
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
 
-        document.getElementById("img").src = document.getElementById('search').value
-        document.getElementById("name").innerHTML = data.docs[0].title_english + " / " + data.docs[0].title_native
-        document.getElementById("episode").innerHTML = data.docs[0].episode
+            console.log(data.docs[0])
+            if (data == "No image received" || data == "Error reading imagenull") {
+                document.getElementById("error").innerHTML = "Error: Your URL is not a valid image";
+                document.getElementById("error").style.display = "block";
+            }
 
-        let minutes = Math.floor(data.docs[0].at / 60)
-        let seconds = data.docs[0].at - minutes * 60;
-        document.getElementById("appear").innerHTML = `${minutes}min:${Math.round(seconds)}s`
+            document.getElementById("img").src = document.getElementById('search').value
+            document.getElementById("name").innerHTML = data.docs[0].title_english + " / " + data.docs[0].title_native
+            document.getElementById("episode").innerHTML = data.docs[0].episode
 
-        document.getElementById("items").style.visibility = "visible";
-    })
-    .catch(err => {
-        document.getElementById("error").innerHTML = "Error: " + err;
-        document.getElementById("error").style.display = "block";
-    })
+            let minutes = Math.floor(data.docs[0].at / 60)
+            let seconds = data.docs[0].at - minutes * 60;
+            document.getElementById("appear").innerHTML = `${minutes}min:${Math.round(seconds)}s`
+
+            document.getElementById("items").style.visibility = "visible";
+        })
+        .catch(err => {
+            document.getElementById("error").innerHTML = "Error: " + err;
+            document.getElementById("error").style.display = "block";
+        })
 }
 
 /*function searchImg(){
