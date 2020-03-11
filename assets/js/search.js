9ccb2b2aebb1f3f76a8ef44ba62f477d34a5b951
hernikplays <hernik27@gmail.com>
2020-03-11 22:32:48 +0100

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -152,7 +152,7 @@ function searchLink(link) {
             let seconds = data.docs[0].at - minutes * 60;
             document.getElementById("appear").innerHTML = `${minutes}min:${Math.round(seconds)}s`
             //If you DON'T want to make the name a link to auto search data, add // to the start of the following line
-            document.getElementById("link").href = `https://yourwebsite.com/aniget/?search=${data.docs[0].title_english}`
+            document.getElementById("link").href = `https://aniget.hernikplays.cz/?search=${data.docs[0].title_english}`
             document.getElementById("items").style.visibility = "visible";
         })
         .catch(err => {
